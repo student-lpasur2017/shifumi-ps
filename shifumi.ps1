697ef3bf873df2228fb5ffa1c67c667a38e44b69
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-12 17:15:07 +0200

    Premiere version algorithme

---
+++ shifumi.ps1
@@ -0,0 +1,26 @@
+# Initialisation des variables
+
+# Message d'accueil
+
+# Combien de joueur humain ?
+
+# Si au moins un humain, creer le joueur et l'accueillir
+
+# Sinon, creer le joueur machine et l'accueillir
+
+# Qui commence aleatoirement
+
+# Combien de manche ?
+
+# Affichage des regles du jeu
+
+# Boucle pour  le jeu
+# Tant que nbmanche != 0 on continue de joueur
+
+  # clear : pour que le joueur suivant ne voit pas le choix pas le choix du joueur precedent
+
+  # Test d'egalite entre les differents choix des utilisateurs
+
+# fin tant que
+
+# Afficher le gagnant
